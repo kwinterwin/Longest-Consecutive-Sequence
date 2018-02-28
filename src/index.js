517f1347259e78d673b496e93a8fa816e377508a
Samuseva Anna <samuseva.a.s@mail.ru>
2018-02-28 23:50:53 +0300

    Intermediate result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+function ShellSort(array)
+{
+    var n = array.length, i = Math.floor(n/2);
+    while (i > 0)
+     { for (var j = 0; j < n; j++)
+        { var k = j, t = array[j];
+          while (k >= i && array[k-i] > t)
+           { array[k] = array[k-i]; k -= i; }
+           array[k] = t;
+        }
+      i = (i==2) ? 1 : Math.floor(i*5/11);
+     }
+    return array;
+}
+
+function ArrayMax( array ){
+        return Math.max.apply( Math, array );
+};
+
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+    if(array.length==0) return 0;
+
+    var sequenceLength = [];
+    array = ShellSort(array);
+    count = 1;
+    for(var i=0; i<array.length; i++){
+        if((array[i]==array[i+1]-1)){
+          count++;
+        }
+        else if(count!=1){
+            sequenceLength.push(count);
+            count=1;
+        }
+    }
+    if (sequenceLength.length==0) return 1;
+    var count = ArrayMax(sequenceLength);
+    return count;
 }
